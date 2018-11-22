32549c70aa81009b80e7e882066ebaa35f00c847
Geoff Brookshire <g.brookshire@bham.ac.uk>
2018-11-22 17:30:10 +0000

    Initial commit

---
+++ sbatch_submit.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+"""
+Submit a batch job to SLURM on BlueBear.
+"""
+
+import subprocess
+import tempfile
+import argparse
+
+def main():
+    # Read in the template file
+    with open('sbatch_template.sh') as f:
+        template = f.readlines()
+    template = ''.join(template)
+
+    args = [['n', 'ntasks', 'Number of tasks', 1, {}],
+            ['k', 'nodes', 'Number of nodes', 1, {}],
+            ['t', 'time', 'Maximum time to let the job run', '5:0:0', {}],
+            ['m', 'mem', 'Memory allocation', '10G', {}],
+            ['q', 'qos', 'Select a QOS on BlueBear', 'bbdefault',
+             {'choices': ['bbdefault', 'bbfast']}],
+            ['i', 'input', 'Script to run as a Slurm job', None,
+             {'required': True}]]
+
+    # Make the arg parser
+    desc = 'Submit Matlab scripts as Slurm jobs'
+    parser = argparse.ArgumentParser(description=desc)
+    for arg in args:
+        parser.add_argument('-' + arg[0],
+                            '--' + arg[1],
+                            help=arg[2],
+                            default=arg[3],
+                            **arg[4])
+    args = parser.parse_args()
+
+    # Replace the parameters in the sbatch script
+    for arg in ['ntasks', 'nodes', 'time', 'mem', 'qos', 'input']:
+        template = template.replace('_' + arg, str(getattr(args, arg)))
+
+    # Write a temporary sbatch script
+    tf = tempfile.NamedTemporaryFile()
+    tf.writelines(template)
+
+    # Run the job
+    p = subprocess.call('sbatch ' + temp_filename, shell=True)
+
+
+if __name__ == "__main__":
+   main()
---
+++ sbatch_template.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#SBATCH --ntasks _ntasks
+#SBATCH --nodes _nodes
+#SBATCH --time _time
+#SBATCH --mem _mem
+#SBATCH --qos _qos
+
+#### This template is filled in by sbatch_submit.py
+
+set -e
+
+module purge; module load bluebear
+module load apps/matlab/r2017a
+
+# # Set up scratch space
+# BB_WORKDIR=$(mktemp -d /scratch/${USER}_${SLURM_JOBID}.XXXXXX)
+# export TMPDIR=${BB_WORKDIR}
+
+matlab -nodisplay -r _input
+
+# # Copy your files back to the current directory
+# test -d ${BB_WORKDIR} && /bin/cp -r ${BB_WORKDIR} ./
+
+# # Delete the temporary files
+# test -d ${BB_WORKDIR} && /bin/rm -rf ${BB_WORKDIR}
