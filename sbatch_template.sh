2e13db61cdf9651eccc5c5f1e682175c37ef8737
gbrookshire <g.brookshire@bham.ac.uk>
2019-02-06 16:11:46 +0000

    Deleted unused code about scratch space and temporary files

--- sbatch_template.sh
+++ sbatch_template.sh
@@ -5,21 +5,14 @@
 #SBATCH --qos _qos
 #SBATCH --error=slurm-%j.err
 
-#### This template is filled in by sbatch_submit.py
+##################################################
+# This template is filled in by sbatch_submit.py #
+##################################################
 
 set -e
 
 module purge; module load bluebear
 module load apps/matlab/r2017a
 
-# # Set up scratch space
-# BB_WORKDIR=$(mktemp -d /scratch/${USER}_${SLURM_JOBID}.XXXXXX)
-# export TMPDIR=${BB_WORKDIR}
-
 matlab -nodisplay -r _input
 
-# # Copy your files back to the current directory
-# test -d ${BB_WORKDIR} && /bin/cp -r ${BB_WORKDIR} ./
-
-# # Delete the temporary files
-# test -d ${BB_WORKDIR} && /bin/rm -rf ${BB_WORKDIR}
